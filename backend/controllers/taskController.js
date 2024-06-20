c8a3f5760dd3b9714373d9ec676b500c2bc60553
AnicetJonhia <anicet22.aps2a@gmail.com>
2024-06-20 11:42:24 +0300

    backend finished

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,127 +1,105 @@
 const Task = require("../models/taskModel");
-const jwt = require("jsonwebtoken");
-
 const { notifyIfOverdue } = require("../utils/notifyIfOverdue");
 
-// get all tasks for the authenticated user
+// Créer une tâche pour l'utilisateur authentifié
+exports.createTask = async (req, res) => {
+    const userId = req.user.userId; // Utilisez l'ID utilisateur extrait par le middleware
+    const task = new Task({ ...req.body, userId });
+
+    try {
+        await task.save();
+        res.status(201).json({ task });
+    } catch (error) {
+        console.error("Error creating task:", error);
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Obtenir toutes les tâches de l'utilisateur authentifié
 exports.getAllTasks = async (req, res) => {
     try {
-        const userId = req.user._id;
-        const tasks = await Task.find({ userId });
+        const userId = req.user.userId;
+        // const tasks = await Task.find({ userId: req.user._id });
+        const tasks = await Task.find({  userId: userId}).sort({createdAt : -1});
 
-        // Génère un jeton d'accès JWT avec le nom de l'utilisateur
-        const accessToken = jwt.sign({ username: req.user.name }, process.env.JWT_SECRET);
-
-        // Envoi de la réponse avec les tâches et le jeton
-        res.status(200).json({
-            tasks,
-            accessToken
-        });
-
+        res.status(200).json({ tasks });
     } catch (error) {
-        res.status(500).send();
-    }
-};
-
-// get all tasks by status for the authenticated user
-exports.getTasksByStatus = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const tasks = await Task.find({ userId, status: req.params.status });
-        res.json(tasks);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-};
-
-// get tasks number by status for the authenticated user
-exports.getTasksCount = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const todoCount = await Task.countDocuments({ userId, status: 'Todo' });
-        const inProgressCount = await Task.countDocuments({ userId, status: 'In progress' });
-        const completedCount = await Task.countDocuments({ userId, status: 'Completed' });
-
-        // Génère un jeton d'accès JWT avec le nom de l'utilisateur
-        const accessToken = jwt.sign({ username: req.user.name }, process.env.JWT_SECRET);
-
-        res.json({
-            todoCount ,
-            inProgressCount,
-            completedCount,
-            accessToken
-        });
-    } catch (error) {
-        console.log("Getting tasks count", error);
+        console.error("Error getting tasks:", error);
         res.status(500).json({ message: error.message });
     }
 };
 
-// Create task for the authenticated user
-exports.createTask = async (req, res) => {
-    const userId = req.user._id;
-    console.log('Creating task for user:', userId);
-    const task = new Task({ ...req.body, userId });
+// Obtenir toutes les tâches par statut pour l'utilisateur authentifié
+exports.getTasksByStatus = async (req, res) => {
     try {
-        // Génère un jeton d'accès JWT avec le nom de l'utilisateur
-        const accessToken = jwt.sign({ username: req.user.name }, process.env.JWT_SECRET);
-        await task.save();
-        // Envoi de la réponse avec les tâches et le jeton
-        res.status(200).json({
-            task,
-            accessToken
-        });
+        const userId = req.user.userId;
+        const tasks = await Task.find({ userId: userId, status: req.params.status }).sort({createdAt : -1});
+
+        res.status(200).json({ tasks });
     } catch (error) {
-        console.log("Error creating task:",error)
-        res.status(400).send(error);
+        console.error("Error getting tasks by status:", error);
+        res.status(500).json({ message: error.message });
     }
 };
 
-// Update task for the authenticated user
+// Obtenir le nombre de tâches par statut pour l'utilisateur authentifié
+exports.getTasksCount = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const todoCount = await Task.countDocuments({ userId: userId, status: 'Todo' });
+        const inProgressCount = await Task.countDocuments({ userId: userId, status: 'In progress' });
+        const completedCount = await Task.countDocuments({ userId: userId, status: 'Completed' });
+        res.status(200).json({ todoCount, inProgressCount, completedCount });
+    } catch (error) {
+        console.error("Error getting tasks count:", error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Mettre à jour une tâche pour l'utilisateur authentifié
 exports.updateTask = async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['title', 'description', 'category', 'priority', 'dueDate', 'status', 'completed'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
 
     if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' });
+        return res.status(400).json({ error: 'Invalid updates!' });
     }
 
     try {
-        const userId = req.user._id;
-        const task = await Task.findOneAndUpdate({ _id: req.params.id, userId }, req.body, { new: true, runValidators: true });
+        const userId = req.user.userId; // Utilisez l'ID utilisateur extrait par le middleware
+        const task = await Task.findOneAndUpdate(
+            { _id: req.params.id, userId: userId },
+            req.body,
+            { new: true, runValidators: true }
+        );
 
         if (!task) {
-            return res.status(404).send();
+            return res.status(404).json({ message: 'Task not found' });
         }
-        await notifyIfOverdue(task); // Vérifier et notifier si la tâche est en retard
 
-        // Génère un jeton d'accès JWT avec le nom de l'utilisateur
-        const accessToken = jwt.sign({ username: req.user.name }, process.env.JWT_SECRET);
+        await notifyIfOverdue(task); // Notifier si la tâche est en retard
 
-        // Envoi de la réponse avec les tâches et le jeton
-        res.status(200).json({
-            task,
-            accessToken
-        });
-        res.send(task);
+        res.status(200).json({ task });
     } catch (error) {
-        res.status(400).send(error);
+        console.error("Error updating task:", error);
+        res.status(400).json({ message: error.message });
     }
 };
 
-// Delete task for the authenticated user
+// Supprimer une tâche pour l'utilisateur authentifié
 exports.deleteTask = async (req, res) => {
     try {
-        const userId = req.user._id;
-        const task = await Task.findOneAndDelete({ _id: req.params.id, userId });
+        const userId = req.user.userId;
+        const task = await Task.findOneAndDelete({ _id: req.params.id, userId : userId});
 
         if (!task) {
-            return res.status(404).send();
+            return res.status(404).json({ message: 'Task not found' });
         }
 
-        res.send(task);
+        res.status(200).json({ msg: "Task deleted " });
     } catch (error) {
-        res.status(500).send();
+        console.error("Error deleting task:", error);
+        res.status(500).json({ message: error.message });
     }
 };
